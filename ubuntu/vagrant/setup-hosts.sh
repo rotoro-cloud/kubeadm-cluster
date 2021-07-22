633c399325543c6e7f02cdab343164d940d703a5
Rotoro.cloud <72907533+rotoro-cloud@users.noreply.github.com>
2021-07-22 13:02:44 +0200

    Update setup-hosts.sh

--- ubuntu/vagrant/setup-hosts.sh
+++ ubuntu/vagrant/setup-hosts.sh
@@ -9,10 +9,10 @@ sed -e '/^.*ubuntu-bionic.*/d' -i /etc/hosts
 
 # Update /etc/hosts about other hosts
 cat >> /etc/hosts <<EOF
-192.168.66.11  master-1
-192.168.66.12  master-2
-192.168.66.13  master-3
-192.168.66.21  worker-1
-192.168.66.22  worker-2
+192.168.66.11  controlplane01
+192.168.66.12  controlplane02
+192.168.66.13  controlplane03
+192.168.66.21  node01
+192.168.66.22  node02
 192.168.66.30  lb
 EOF
