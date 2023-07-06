843bce1a4407fb7abddd6c23aa61f55429fd79f6
Rotoro.cloud <72907533+rotoro-cloud@users.noreply.github.com>
2023-07-06 10:46:35 +0300

    Update setup-hosts.sh

--- ubuntu/vagrant/setup-hosts.sh
+++ ubuntu/vagrant/setup-hosts.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
-set -e
+#!/bin/bash
+set -ex
 IFNAME=$1
+THISHOST=$2
 ADDRESS="$(ip -4 addr show $IFNAME | grep "inet" | head -1 |awk '{print $2}' | cut -d/ -f1)"
+NETWORK=$(echo $ADDRESS | awk 'BEGIN {FS="."} ; { printf("%s.%s.%s", $1, $2, $3) }')
 sed -e "s/^.*${HOSTNAME}.*/${ADDRESS} ${HOSTNAME} ${HOSTNAME}.local/" -i /etc/hosts
 
-# remove ubuntu-bionic entry
-sed -e '/^.*ubuntu-bionic.*/d' -i /etc/hosts
+# remove ubuntu-jammy entry
+sed -e '/^.*ubuntu-jammy.*/d' -i /etc/hosts
+sed -e "/^.*$2.*/d" -i /etc/hosts
 
 # Update /etc/hosts about other hosts
 cat >> /etc/hosts <<EOF
-192.168.66.11  controlplane01
-192.168.66.12  controlplane02
-192.168.66.13  controlplane03
-192.168.66.21  node01
-192.168.66.22  node02
-192.168.66.30  lb
+${NETWORK}.11  controlplane01
+${NETWORK}.12  controlplane02
+${NETWORK}.13  controlplane03
+${NETWORK}.21  node01
+${NETWORK}.22  node02
+${NETWORK}.23  node03
+${NETWORK}.30  lb
 EOF
